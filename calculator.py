940a9f0f9b8d969ed1ce23dd99558f5fafa7f029
xDarxy <123395855+xDarxy@users.noreply.github.com>
2023-03-26 13:20:34 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+number1 = float(input("Choose a number: "))
+operator = input("choose an operator: ")
+number2 = float(input("Choose a number: "))
+
+if (operator == "+"):
+    print(number1 + number2)
+
+if (operator == "-"):
+    print(number1 - number2)
+
+if (operator == "*"):
+    print(number1 * number2)
+
+if (operator == "/"):
+    print(number1 / number2)
